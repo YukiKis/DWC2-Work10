2ef5a8a4995437faaed64d150d7d022fe8dec45c
EC2 Default User <ec2-user@ip-172-31-47-28.ec2.internal>
2020-10-04 12:20:43 +0000

    [FINISH] 10

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,6 +19,7 @@
 
 require File.expand_path(File.dirname(__FILE__) + "/environment")
 rails_env = Rails.env.to_sym
+set :environment, rails_env
 set :output, "log/cron.log"
 every :day do
   begin
--- lib/batch/send_mail.rb
+++ lib/batch/send_mail.rb
@@ -2,7 +2,7 @@ class Batch::SendMail
   def self.send_daily_mail
     User.all.each do |user|
       NotificationMailer.daily_mail(user).deliver_now
-      p "Finish sending daily mails" 
     end
+    p "Finish sending daily mails" 
   end
 end
